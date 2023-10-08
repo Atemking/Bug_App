c69fd58fe3e77331d78522292752975f11c15afe
Atemafackingsley <135429803+Atemking@users.noreply.github.com>
2023-10-08 16:48:29 +0300

    Third Task commit

--- bugs/urls.py
+++ bugs/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "bugs"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("contact/",views.contact_page_view , name = "contact_page_view"),
+    path("register", views.register, name="register"),
+    path("display", views.display, name="display"),
+    path('bugs/<int:data_id>', views.info, name="info"),
+    
 ]
