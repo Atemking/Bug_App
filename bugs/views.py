d1d7925569f67dea64e02c7fa673c8c09db389af
Atemafackingsley <135429803+Atemking@users.noreply.github.com>
2023-10-11 10:12:20 +0300

    update on task 3

--- bugs/views.py
+++ bugs/views.py
@@ -8,6 +8,9 @@ from bugs.forms import registerbugs
 from bugs.models import Bugs
 from .models import Bugs
 
+import django_tables2 as tables 
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the bugs index.")
 
@@ -16,7 +19,7 @@ def contact_page_view(request):
     return HttpResponse("This is the contact page")
 
 def register(request):
-	context ={}
+	context = {}
 
 	# create object of form
 	form = registerbugs(request.POST or None, request.FILES or None)
@@ -27,17 +30,26 @@ def register(request):
 		form.save()
 
 	context['form']= form
-	return render(request, "register.html", context)
+	return render(request, "bugs/register.html", context)
 
 
-def display(request):
-    data = Bugs.objects.all()
-    return render(request,'bugs/index.html',{'datas': data })
+# def display(request):
+   # data = Bugs.objects.all()
+    #return render(request,'bugs/index.html',{'datas': data })
 
-def info(request,data_id):
+# def info(request,data_id):
     #datar = Bugs.objects.get(pk = data_id)
-    datar = Bugs._meta.get_fields(data_id) 
+   # datar = Bugs._meta.get_fields(data_id) 
     
-    return render(request,'bugs/info.html',{'datarrr': datar })
+    #return render(request,'bugs/info.html',{'datarrr': datar })
 
-    
+# this class will create the table just like how we create forms
+class SimpleTable(tables.Table):
+   class Meta:
+      model = Bugs
+
+# this will render table
+class TableView(tables.SingleTableView):
+   table_class = SimpleTable
+   queryset = Bugs.objects.all()
+   template_name = "table_example.html"
