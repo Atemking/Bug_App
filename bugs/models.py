9ec09eeab8c25ef0f4440fe2cc909a645b6ae296
Atemafackingsley <135429803+Atemking@users.noreply.github.com>
2023-10-14 11:13:02 +0300

    Task 4 update

--- bugs/models.py
+++ bugs/models.py
@@ -2,10 +2,17 @@ from __future__ import unicode_literals
 from django.db import models
 from django.forms import ModelForm
 
+import datetime
+
+from django.utils import timezone
+
 class Bugs(models.Model):
     description = models.CharField(max_length=200)
     bug_type = models.CharField(max_length=20)
     report_date = models.DateTimeField("date reported")
     status = models.CharField(max_length=30)
 
-
+# checking for test 
+def report_date(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
